24f0c210ea07610b5c0d25fe5e2216f25fba104f
kubwimanav <kubwimanavincent2020@gmail.com>
2024-06-07 18:36:15 +0200

    ui-challrnge

---
+++ ui-challenge/js/index.js
@@ -0,0 +1,50 @@
+
+// Array containing the gallery data
+const galleryData = [
+    {
+        imgSrc: "./assets/image4.png",
+        title: "FENNEC FOX",
+        location: "India",
+    },
+    {
+        imgSrc: "./assets/image 4.png",
+        title: "HUMPBACK WHALE",
+        location: "south africa",
+    },
+    {
+        imgSrc: "./assets/Image.png",
+        title: "COMMON_BROWN BABOON",
+        location: "south africa"
+    },
+    {
+        imgSrc: "./assets/Image3.png",
+        title: "SPOTTED DEER",
+        location: "amazon",
+    },
+];
+
+// Function to create gallery cards
+function createGalleryCards() {
+    const galleryWrapper = document.querySelector(".galleryWrapper");
+    galleryData.forEach((data) => {
+        const galleryCard = document.createElement("div");
+        galleryCard.classList.add("galleryCard");
+
+        galleryCard.innerHTML = `
+    <img src="${data.imgSrc}" alt="${data.title}" />
+    <div class="galleryOverlay">
+      <div class="contents">
+        <h6>${data.title.split(" ").join("<br>")}</h6>
+        <small class="location">${data.location}</small>
+        <p class="more"> <span> Know More </span> <i class="fa-solid fa-arrow-right-long"></i></p>
+        
+      </div>
+    </div>
+  `;
+
+        galleryWrapper.appendChild(galleryCard);
+    });
+}
+
+// Call the function to create gallery cards on page load
+document.addEventListener("DOMContentLoaded", createGalleryCards);
